55b83c13a1db8b16f896dee27db3130b4692becb
Elizabeth <83429100+Elizabeth1073@users.noreply.github.com>
2021-05-09 14:37:07 +0300

    Add files via upload
    added task1

--- Program.cs
+++ Program.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _3lessonHW
+namespace task1
 {
-    //Дописать структуру Complex, добавив метод вычитания комплексных чисел.Продемонстрировать работу структуры.
-
-    struct Complex
+    class Complex
     {
+//        Дописать класс Complex, добавив методы вычитания и произведения чисел.Проверить работу класса.
+//в) Добавить диалог с использованием switch демонстрирующий работу класса.
+
         public double re;
         public double im;
 
@@ -19,12 +20,12 @@ namespace _3lessonHW
             this.im = im;
         }
 
-        public static Complex Substract(Complex q, Complex w)
+        public Complex Substract(Complex q, Complex w)
         {
             return new Complex(q.re - w.re, q.im - q.re);
         }
 
-        public static Complex Mult(Complex q, Complex w)
+        public Complex Mult(Complex q, Complex w)
         {
             return new Complex(q.re * w.re, q.im * w.im);
         }
@@ -37,42 +38,17 @@ namespace _3lessonHW
         {
             Console.WriteLine($"{re} * {im}i");
         }
-        //public void Create()
-        //{
-        //    Complex z1 = new Complex(re: 123, im: 45);
-        //    Complex z2 = new Complex(re: 3, im: 67);
-        //    Complex z3 = Complex.Substract(z1, z2);
-        //}
-
     }
-
     class Program
     {
-
         static void Main(string[] args)
         {
-            Complex z1 = new Complex(re: 123, im: 45);
-            Complex z2 = new Complex(re: 3, im: 67);
-            Complex z3 = new Complex();
-
-            Console.WriteLine("Добро пожаловать в калькулятор вещественных чисел! Для того, чтобы начать выберите операцию: \nвычитание\nумножение");
-            string user = Console.ReadLine();
-            switch (user)
-            {
-                case "вычитание":
-                    z3 = Complex.Substract(z1, z2);
-                    z3.PrintS();
-                    break;
-                case "умножение":
-                    z3 = Complex.Mult(z1, z2);
-                    z3.PrintM();
-                    break;
-                default:
-                    Console.WriteLine("Ошибка, введены некорректные данные");
-                    break;
-            }
-
-
+            Complex Comp = new Complex(2, 2);
+            Complex Comp1 = new Complex(3, 3);
+            Comp.Substract(Comp, Comp1);
+            Comp.PrintS();
+            Comp.Mult(Comp, Comp1);
+            Comp.PrintM();
 
             Console.ReadLine();
         }
